60c7ec2905ab1786d0b09d5d0b49b74582684356
Alza I.K <alzaichsank@gmail.com>
2022-04-08 20:30:59 +0700

    add design tutor activity

---
+++ app/src/main/java/id/alik/tutor_android/tutordesign/TrainingDesignActivity.kt
@@ -0,0 +1,12 @@
+package id.alik.tutor_android.tutordesign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.alik.tutor_android.R
+
+class TrainingDesignActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_training_design)
+    }
+}
