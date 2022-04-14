f6d706b63afed231e3f378b2da4f6405a1c7542e
Alza I.K <alzaichsank@gmail.com>
2022-04-14 22:20:26 +0700

    make flow for all case

--- app/src/main/java/id/alik/tutor_android/tutordesign/intentlearning/IntentFirstActivity.kt
+++ app/src/main/java/id/alik/tutor_android/tutordesign/intentlearning/IntentFirstActivity.kt
@@ -14,20 +14,45 @@ class IntentFirstActivity : AppCompatActivity() {
     }
 
     private fun setUpView() {
+
         with(mBinding) {
             btnIntent.setOnClickListener {
-
+                setUpData(INTENT_EXTRA)
             }
             btnIntentBundle.setOnClickListener {
-
+                setUpData(BUNDLE_EXTRA)
             }
-            btnIntentSerializable.setOnClickListener { }
+            btnIntentSerializable.setOnClickListener {
+                setUpData(SERIALIZABELE_EXTRA)
+            }
 
             btnIntentParcelabel.setOnClickListener {
-
+                setUpData(PARCELABEL_EXTRA)
             }
-
-
         }
+    }
+
+    private fun setUpData(type: String) {
+        when (type) {
+            INTENT_EXTRA -> {
+                //TODO add case for intent
+            }
+            BUNDLE_EXTRA -> {
+                //TODO add case for bundle
+            }
+            SERIALIZABELE_EXTRA -> {
+                //TODO add case for serializabele
+            }
+            PARCELABEL_EXTRA -> {
+                //TODO add case for parcelable
+            }
+        }
+    }
+
+    companion object {
+        const val INTENT_EXTRA = "intent.key"
+        const val BUNDLE_EXTRA = "bundle.key"
+        const val SERIALIZABELE_EXTRA = "serializabele.key"
+        const val PARCELABEL_EXTRA = "parcelable.key"
     }
 }
