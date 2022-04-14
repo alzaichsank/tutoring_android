5b84a1bb8e541ef36264c654df9503cbbfa5e4ad
Alza Ichsan Kurniawan <alza.kurniawan@sehatq.com>
2022-04-14 21:57:05 +0700

    add parcelize, add text and button intent

---
+++ app/src/main/java/id/alik/tutor_android/tutordesign/intentlearning/model/PersonIntentParcelable.kt
@@ -0,0 +1,13 @@
+package id.alik.tutor_android.tutordesign.intentlearning.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class PersonIntentParcelable(
+    var nama: String,
+    var umur: Int,
+    var email: String,
+    var domisili: String,
+    var statusMenikah: String,
+) : Parcelable
---
+++ app/src/main/java/id/alik/tutor_android/tutordesign/intentlearning/model/PersonIntentSerializable.kt
@@ -0,0 +1,11 @@
+package id.alik.tutor_android.tutordesign.intentlearning.model
+
+import java.io.Serializable
+
+class PersonIntentSerializable(
+    var nama: String,
+    var umur: Int,
+    var email: String,
+    var domisili: String,
+    var statusMenikah: String,
+) : Serializable
