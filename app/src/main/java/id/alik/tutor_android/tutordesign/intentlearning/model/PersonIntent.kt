fb12cb0605f0b412ba382e6d594c652954a9c21e
Alza Ichsan Kurniawan <alza.kurniawan@sehatq.com>
2022-04-14 21:43:35 +0700

    init case intent tutor

---
+++ app/src/main/java/id/alik/tutor_android/tutordesign/intentlearning/model/PersonIntent.kt
@@ -0,0 +1,8 @@
+package id.alik.tutor_android.tutordesign.intentlearning.model
+
+class PersonIntent(var nama: String,
+                   var umur: Int,
+                   var email: String,
+                   var domisili: String,
+                   var statusMenikah: String,
+)
