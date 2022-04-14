75a1b8705d182b5851c2f987c800ee7d57c33785
Alza I.K <alzaichsank@gmail.com>
2022-04-14 21:43:35 +0700

    init case intent tutor

---
+++ app/src/main/java/id/alik/tutor_android/tutordesign/intentlearning/IntentSecondsActivity.kt
@@ -0,0 +1,12 @@
+package id.alik.tutor_android.tutordesign.intentlearning
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.alik.tutor_android.R
+
+class IntentSecondsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intent_two)
+    }
+}
