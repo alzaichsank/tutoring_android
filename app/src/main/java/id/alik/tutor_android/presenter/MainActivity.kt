bf64414358ef6bac4262425f90eec4dc50dcb12f
Alza I.K <alzaichsank@gmail.com>
2022-04-17 23:31:20 +0700

    change from button to list item

--- app/src/main/java/id/alik/tutor_android/presenter/MainActivity.kt
+++ app/src/main/java/id/alik/tutor_android/presenter/MainActivity.kt
@@ -2,12 +2,16 @@ package id.alik.tutor_android.presenter
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import id.alik.tutor_android.databinding.ActivityMainBinding
+import id.alik.tutor_android.presenter.adapter.DemoListAdapter
 import id.alik.tutor_android.service.NavigationService
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var navigationService: NavigationService? = null
+    lateinit var demoListAdapter: DemoListAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -17,10 +21,31 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setUpView() {
-        with(binding) {
-            btnIntent.setOnClickListener {
-                navigationService?.showIntentCase(this@MainActivity)
+        demoListAdapter = DemoListAdapter(
+            arrayListOf(
+                TUTOR_INTENT,
+                TUTOR_VIEW_PAGER
+            )
+        )
+        { tutorName ->
+            when (tutorName) {
+                TUTOR_INTENT -> {
+                    navigationService?.showIntentCase(this@MainActivity)
+                }
+                TUTOR_VIEW_PAGER -> {
+                    //TODO : ADD intent
+                }
             }
         }
+
+        with(binding.rvDemoList) {
+            layoutManager = LinearLayoutManager(this.context, RecyclerView.VERTICAL, false)
+            adapter = demoListAdapter
+        }
+    }
+
+    private companion object {
+        const val TUTOR_INTENT = "Tutor Intent"
+        const val TUTOR_VIEW_PAGER = "Tutor View Pager"
     }
 }
