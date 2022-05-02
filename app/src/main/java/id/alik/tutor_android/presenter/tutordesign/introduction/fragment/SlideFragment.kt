571f9df31faa757f881a7f213b02a2584d980b7a
Alza I.K <alzaichsank@gmail.com>
2022-05-02 22:12:56 +0700

    binding sldie fragment

--- app/src/main/java/id/alik/tutor_android/presenter/tutordesign/introduction/fragment/SlideFragment.kt
+++ app/src/main/java/id/alik/tutor_android/presenter/tutordesign/introduction/fragment/SlideFragment.kt
@@ -5,9 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import id.alik.tutor_android.R
+import id.alik.tutor_android.databinding.FragmentSlideBinding
 
 class SlideFragment : Fragment() {
+    private var mPage: Int = 0
+    private lateinit var binding: FragmentSlideBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,8 +19,9 @@ class SlideFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_slide, container, false)
+        binding = FragmentSlideBinding.inflate(layoutInflater)
+        
+        return binding.root
     }
 
     companion object {
